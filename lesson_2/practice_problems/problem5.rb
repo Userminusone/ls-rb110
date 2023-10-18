7c87a0cb15a3bf65b62a042c3e8d17120edbe91c
Joseph Sheasby <sheasbyj@icloud.com>
2023-10-17 22:24:19 -0500

    added answers for the working with blocks assignment, and added first few problems for lesson 2 practice problems

---
+++ lesson_2/practice_problems/problem5.rb
@@ -0,0 +1,15 @@
+munsters = {
+  "Herman" => { "age" => 32, "gender" => "male" },
+  "Lily" => { "age" => 30, "gender" => "female" },
+  "Grandpa" => { "age" => 402, "gender" => "male" },
+  "Eddie" => { "age" => 10, "gender" => "male" },
+  "Marilyn" => { "age" => 23, "gender" => "female"}
+}
+
+total_male_age = munsters.select do |key, value|
+                   value['gender'] == 'male'
+                 end.map do |key, value|
+                   value['age']
+                 end.sum
+
+p total_male_age
